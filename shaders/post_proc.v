655c4cdf70665aea149c1473b10d337ebb55390b
Martin Sarajærvi <balony@gmail.com>
2014-07-13 19:10:39 +0200

    added shader

---
+++ shaders/post_proc.v
@@ -0,0 +1,13 @@
+#version 330
+
+layout(location = 0) in vec3 vertex_position;
+
+out vec2 st;
+
+void main()
+{
+  st = (vec2(vertex_position) + 1.0) * 0.5;
+  gl_Position = vec4(vertex_position, 1.0);
+}
+
+
