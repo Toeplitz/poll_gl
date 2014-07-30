40315ab20df7fd706036315682a4deba3bcd7a61
Martin Sarajærvi <balony@gmail.com>
2014-07-30 13:26:14 +0200

    add stencil shader

---
+++ shaders/stencil_pass.v
@@ -0,0 +1,66 @@
+#version 330
+
+layout(location = 0) in vec3 vertex_position;
+layout(location = 1) in vec3 vertex_normal;
+layout(location = 2) in vec3 vtangent;
+layout(location = 3) in vec3 bitangent;
+layout(location = 4) in vec3 weights;
+layout(location = 5) in ivec3 bone_index;
+layout(location = 6) in vec2 texture_coord;
+
+
+layout(std140) uniform GlobalMatrices {
+  mat4 proj;
+  mat4 inv_proj;
+  mat4 view;
+};
+
+layout(std140) uniform Matrices {
+  mat4 model;
+};
+
+layout(std140) uniform Armature {
+  mat4 skinning[64];
+};
+
+layout(std140) uniform Node_State {
+  int state_animated;
+  int state_debug;
+  int state_diffuse;
+  int state_diffuse_normal;
+  int state_diffuse_specular_normal;
+  int state_cubemap_reflect;
+  int state_cubemap_skybox;
+  int state_standard;
+};
+
+
+
+mat4 func_animation_matrix_get()
+{
+  mat4 animation = 
+    weights[0] * skinning[int(bone_index[0])] +
+    weights[1] * skinning[int(bone_index[1])] +
+    weights[2] * skinning[int(bone_index[2])];
+
+  //if (weights[0] == 0.0 && weights[1] == 0.0 && weights[2] == 0.0) {
+  //  animation = model;
+  //}
+
+  return animation;
+}
+
+
+void main(void) 
+{
+  mat4 m = model;
+  if (state_animated == 1) {
+    m = model * func_animation_matrix_get();
+  }
+  
+  mat4 model_view = view * m;
+
+  // Out variables
+  vec3 position_eye = (model_view * vec4(vertex_position, 1.0)).xyz;
+  gl_Position = proj * vec4(position_eye, 1.0);
+}
