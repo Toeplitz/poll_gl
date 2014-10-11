3f8074eacf77c1a2ef6f3c718a06c52249c0d9c4
Martin Sarajærvi <balony@gmail.com>
2014-10-11 12:12:51 +0200

    added plugins to the sponza demo

--- demos/06_sponza/06_sponza.cpp
+++ demos/06_sponza/06_sponza.cpp
@@ -1,6 +1,8 @@
 #include "poll.h"
 #include "plugin_debug.h"
+#include "plugin_light_tool.h"
 #include "plugin_firstperson_cam.h"
+#include "plugin_node_tool.h"
 #include <iostream>
 
 
@@ -11,14 +13,17 @@ int main()
   Scene &scene = poll.scene_get();
 
   Node *camera_node = scene.node_camera_get();
-  auto firstperson_camera = std::unique_ptr<Plugin_Firstperson_Camera>(new Plugin_Firstperson_Camera(poll.console_get(), scene, camera_node));
-  poll.plugin_add(*firstperson_camera);
-
-  auto debug = std::unique_ptr<Plugin_Debug>(new Plugin_Debug(poll.console_get(), scene));
-  poll.plugin_add(*debug);
+  auto plugin_debug = std::unique_ptr<Plugin_Debug>(new Plugin_Debug(poll.console_get(), scene));
+  auto plugin_light_tool = std::unique_ptr<Plugin_Light_Tool>(new Plugin_Light_Tool(poll.console_get(), scene));
+  auto plugin_node_tool = std::unique_ptr<Plugin_Node_Tool>(new Plugin_Node_Tool(poll.console_get(), scene));
+  auto plugin_firstperson_camera = std::unique_ptr<Plugin_Firstperson_Camera>(new Plugin_Firstperson_Camera(poll.console_get(), scene, camera_node));
+  poll.plugin_add(*plugin_debug);
+  poll.plugin_add(*plugin_light_tool);
+  poll.plugin_add(*plugin_firstperson_camera);
+  poll.plugin_add(*plugin_node_tool);
 
   Node &sponza = scene.load("data/crytek-sponza/", "sponza_with_spec.obj", MODEL_IMPORT_DEFAULT);
-  sponza.scale(scene, glm::vec3(0.1, 0.1, 0.1));
+  sponza.scale(scene, glm::vec3(0.05, 0.05, 0.05));
 
   std::vector<std::unique_ptr<Physics_Triangle_Mesh_Shape>> shapes;
   for (auto &child: sponza.children_get()) {
