7b810ebd1029347d622e7b36be15434894a3422a
Martin Sarajærvi <balony@gmail.com>
2014-12-01 18:28:19 +0100

    setyp stylesheet

--- gui/main.cpp
+++ gui/main.cpp
@@ -1,20 +1,27 @@
 #include "window.h"
 #include <QApplication>
+#include <QFile>
 #include <iostream>
 #include "utils.h"
 
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
-    Window w;
+  QApplication a(argc, argv);
+  QFile file(":/qdarkstyle/style.qss");
+  if(file.open(QIODevice::ReadOnly | QIODevice::Text)) {
+    a.setStyleSheet(file.readAll());
+    file.close();
+  }
 
-    POLL_DEBUG(std::cout, "Poll data path: " << POLL_DATA_PATH);
-    POLL_DEBUG(std::cout, "Poll shader path: " << POLL_SHADER_PATH);
+  Window w;
 
-    w.show();
+  POLL_DEBUG(std::cout, "Poll data path: " << POLL_DATA_PATH);
+  POLL_DEBUG(std::cout, "Poll shader path: " << POLL_SHADER_PATH);
+
+  w.show();
 
 
-    return a.exec();
+  return a.exec();
 
 }
